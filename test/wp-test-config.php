9445d1f9cdaabf0daa8df541784ddfaa3cd3e84b
Renan Ivo Martins <renan.imartins@gmail.com>
2010-07-29 03:37:12 -0300

    unit test structure working

--- test/wp-test-config.php
+++ test/wp-test-config.php
@@ -1,12 +1,12 @@
 <?php
 // WordPress test server's URL. Ex: example.com
-define('WP_TEST_DOMAIN', "");
+define('WP_TEST_DOMAIN', "g3wsites.local");
 
 // super-admin username (default: admin)
-define('WP_TEST_USERNAME', "");
+define('WP_TEST_USERNAME', "admin");
 
 // super-admin user_id
 define('WP_TEST_USER_ID', 1);
 
 // super-admin password
-define('WP_TEST_PASSWORD', "");
+define('WP_TEST_PASSWORD', "123456");
