98bd4956d59915a7a8dbe55b183572d3805ef33a
Renan Ivo Martins <renan.imartins@gmail.com>
2010-07-29 02:48:36 -0300

    adding user id to the test config file

--- test/wp-test-config.php
+++ test/wp-test-config.php
@@ -5,5 +5,8 @@ define('WP_TEST_DOMAIN', "");
 // super-admin username (default: admin)
 define('WP_TEST_USERNAME', "");
 
+// super-admin user_id
+define('WP_TEST_USER_ID', 1);
+
 // super-admin password
 define('WP_TEST_PASSWORD', "");
